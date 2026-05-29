21f450f855914359bbbd3d64958740e34895a130
AndreaMoreau75 <andreamoreau75@icloud.com>
2026-05-29 12:56:27 +0200

    Delete src directory

--- src/components/ui/button.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export function Button({ className='', variant, children, ...props }) {
-  const base = 'inline-flex items-center justify-center font-medium transition disabled:opacity-50';
-  const outline = variant === 'outline' ? 'border bg-transparent hover:bg-black/5' : '';
-  return <button className={`${base} ${outline} ${className}`} {...props}>{children}</button>;
-}
